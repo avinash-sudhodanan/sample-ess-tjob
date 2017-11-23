e780f069b2b99e4b98cdf626ffc3b6a409160ac0
Avinash <avinash.sudhodanan@gmail.com>
2017-11-23 16:59:25 +0100

    Added selenium hub

--- sample-tjob.py
+++ sample-tjob.py
@@ -1,12 +1,25 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        selenium_hub=os.environ['ET_EUS_API']
+        driver = webdriver.Remote(command_executor=selenium_hub,desired_capabilities={'browserName': 'firefox', 'javascriptEnabled': True})
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("https://github.com")
+        assert "GitHub" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("dzitkowskik")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
