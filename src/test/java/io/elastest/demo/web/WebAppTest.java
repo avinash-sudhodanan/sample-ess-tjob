caa05e15242f23b3728d6b382979ba8385d26875
Avinash <avinash.sudhodanan@gmail.com>
2017-11-24 18:19:21 +0100

    Added missing proxy import

--- src/test/java/io/elastest/demo/web/WebAppTest.java
+++ src/test/java/io/elastest/demo/web/WebAppTest.java
@@ -28,6 +28,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
