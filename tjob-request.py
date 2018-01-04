d1344fba8a84dc6e5a1da4c6be8110c3e8a8b250
avinash-sudhodanan <avinash-sudhodanan@gmail.com>
2018-01-04 12:45:10 +0100

    removed commented out lines

--- tjob-request.py
+++ tjob-request.py
@@ -5,7 +5,7 @@ import sys
 
 class TJob():
 	
-	#proxyurl="http://" + os.environ['ET_SUT_HOST'] + ":8080/"
+	
 	proxyurl="http://" + str(sys.argv[1]) + ":8080/"
 	proxies = {'http':proxyurl, 'https':proxyurl}
 	def test_send_request(self):
@@ -13,15 +13,12 @@ class TJob():
 		response=s.get('http://www.mashable.com/', proxies=self.proxies,verify=False)
 		print response.headers
 		response=s.get('https://www.google.com/', proxies=self.proxies,verify=False)
-		#response=s.get('http://etm:8091/')
+		
 		print response.headers
 		response=s.get('https://www.facebook.com/', proxies=self.proxies,verify=False)
 		print response.headers
 		response=s.get('https://www.amazon.com/', proxies=self.proxies,verify=False)
-		print response.headers
-		#assert "Google" in response.text
-		
+		print response.headers		
 if __name__=="__main__":
 	tjob=TJob()
 	tjob.test_send_request()
-
