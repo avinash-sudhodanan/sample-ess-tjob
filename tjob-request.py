fc582b0a8311876a098e370167df0b113629812d
avinash-sudhodanan <avinash.sudhodanan@gmail.com>
2018-05-11 11:37:16 +0200

    Updated as in dockernash/test-tjob-ess

--- tjob-request.py
+++ tjob-request.py
@@ -1,9 +1,15 @@
 import requests
+from pprint import pprint
 import os
 import sys
 
 class TJob():
-	proxyurl="http://" + str(sys.argv[1]) + ":8080/"
+	essApiUrl=os.environ['ET_ESS_API']
+	if essApiUrl.startswith("http:"):
+		proxyurl="http://" + essApiUrl.lstrip("http://").rstrip(":80/ess/api/r3") + ":8080/"
+	elif essApiUrl.startswith("https://"):
+		proxyurl="http://" + essApiUrl.lstrip("https://").rstrip(":80/ess/api/r3") + ":8080/"
+	#proxyurl="http://" + str(sys.argv[1]) + ":8080/"
 	proxies = {'http':proxyurl, 'https':proxyurl}
 	def test_send_request(self,url):
 		s=requests.Session()
