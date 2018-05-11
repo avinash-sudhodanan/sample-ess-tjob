e9de5e41e3764c0b4e761cfa6c0cf5959fca9d36
avinash-sudhodanan <avinash.sudhodanan@gmail.com>
2018-05-11 12:32:22 +0200

    Updated URL to fix r3 to r4 change

--- tjob-request.py
+++ tjob-request.py
@@ -5,10 +5,11 @@ import sys
 
 class TJob():
 	essApiUrl=os.environ['ET_ESS_API']
+	print("ESS URL is: "+str(essApiUrl))
 	if essApiUrl.startswith("http:"):
-		proxyurl="http://" + essApiUrl.lstrip("http://").rstrip(":80/ess/api/r3") + ":8080/"
+		proxyurl="http://" + essApiUrl.lstrip("http://").rstrip(":80/ess/api/r4") + ":8080/"
 	elif essApiUrl.startswith("https://"):
-		proxyurl="http://" + essApiUrl.lstrip("https://").rstrip(":80/ess/api/r3") + ":8080/"
+		proxyurl="http://" + essApiUrl.lstrip("https://").rstrip(":80/ess/api/r4") + ":8080/"
 	#proxyurl="http://" + str(sys.argv[1]) + ":8080/"
 	proxies = {'http':proxyurl, 'https':proxyurl}
 	def test_send_request(self,url):
@@ -19,6 +20,4 @@ class TJob():
 if __name__=="__main__":
 	tjob=TJob()
 	tjob.test_send_request('http://www.mashable.com/')
-	tjob.test_send_request('https://www.google.com/')
-	tjob.test_send_request('https://www.facebook.com/')
-	tjob.test_send_request('https://www.amazon.com/')
+	tjob.test_send_request('http://www.theplantlist.org/')
