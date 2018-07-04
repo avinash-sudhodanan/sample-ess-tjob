128332e2a94b77b86749700cc624ceb4682f41a1
avinash-sudhodanan <avinash-sudhodanan@gmail.com>
2018-07-04 10:23:58 +0100

    Corrected redundant url

--- fteaching-tjob.py
+++ fteaching-tjob.py
@@ -23,6 +23,10 @@ class PythonOrgSearch(unittest.TestCase):
         options.add_argument('--proxy-server='+ess_mitm_proxy_url+":8080")
         capabilities = options.to_capabilities()
         self.driver = webdriver.Remote(command_executor=eusUrl, desired_capabilities=capabilities)
+	#debug
+	#proxies = {'http': ess_mitm_proxy_url+":8080",'https': ess_mitm_proxy_url+":8080"}
+	#requests.get('http://example.org', proxies=proxies)
+
         #self.driver = webdriver.Chrome(desired_capabilities=capabilities)
         #self.driver = webdriver.Firefox()
     def test_search_in_python_org(self):
@@ -44,13 +48,14 @@ class PythonOrgSearch(unittest.TestCase):
 		time.sleep(5)
 		settings_btn = driver.find_element_by_id("settings-button")
 		settings_btn.click()
-		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://52.50.3.12/#/"]})
+		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://52.50.3.12"]})
 	elif flag == "fullteaching-home":
-		driver.get("https://52.50.3.12/#/")		
-		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://52.50.3.12/#/"]})
+		driver.get("https://52.50.3.12/#/")
+		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://52.50.3.12"]})
 	else:
 		driver.get("https://example.com")
-		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://example.com"]})
+		driver.get("https://example.org")
+		re=requests.post(ess_url+"/ess/api/r4/start/",json={"sites": ["https://example.com","https://example.org"]})
         #Checking the status of the scan
         if "starting-ess" in re.text:
             req=requests.get(ess_url+'/ess/api/r4/status/')
